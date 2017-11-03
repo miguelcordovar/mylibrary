4d172e00fb9559f215b0b0675d4dbfd9bd857b9f
mcordova <miguelcordovar@gmail.com>
2017-11-03 01:45:11 -0500

    Add more code

--- src/test/java/pe/cordova/service/EmailServiceTest.java
+++ src/test/java/pe/cordova/service/EmailServiceTest.java
@@ -1,22 +1,31 @@
 package pe.cordova.service;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class EmailServiceTest {
 
+    private EmailService service;
+
+    @Before
+    public void setup() {
+
+        service = new EmailService();
+
+    }
+
     @Test
-    public void TestEmail() {
+    public void TestEmailGenerator() {
+
+        System.out.println("TestCase - TestEmailGenerator");
 
         String name = "miguel";
         String domain = "tcs.com";
 
         String email = "miguel@tcs.com";
 
-        EmailService service = new EmailService();
-
         Assert.assertEquals(email, service.GenerateEmail(name,domain));
-
 
     }
 
